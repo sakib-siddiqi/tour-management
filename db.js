3417746e816bbd0c9f0aacd91f9e90c7023718a0
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2022-10-18 13:06:23 +0600

    add mongodb

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/tour-management');
+require('dotenv').config();
+const DB_URL=process.env.NODE_ENV="production"?process.env.DB_URL:'mongodb://localhost:27017/tour-management'
+mongoose.connect(DB_URL);
 module.exports.DB=mongoose;
